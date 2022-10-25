2292afa1338a7ce57401736e6aa3087dfbeee44e
EmilsVasilis <emils5264@gmail.com>
2022-10-25 19:42:09 +0100

    Testing CI

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,3 +1,6 @@
 public class hello {
 
+ public static String hello(){
+   return "Hello";
+ }
 }
--- src/test/java/helloTest.java
+++ src/test/java/helloTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 public class helloTest {
+
+
+
+    @Test
+    public void testHello(){
+        assertEquals("Hello", hello.hello());
+    }
 }
