d65999b4fade38214c2c5503deaa4ac9f9127e60
Canonyai <99308100+Canonyai@users.noreply.github.com>
2022-11-01 19:20:52 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println(hello.hello());
-
+   public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		
+//		input = "2*((2+4)+(5+4))"
+		String expression = input.next();
+        System.out.println(calculate(expression));
     }
 
 }
