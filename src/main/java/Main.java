02addb496c4dbe3788dc756d93485dd63abb7237
BollinaPranav <bollinap@tcd.ie>
2022-11-05 23:00:10 +0000

    Implemented all the required functions such as division, brackets, log and exp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	
     public static void main(String[] args){
     	Scanner input = new Scanner(System.in);
-        System.out.println("Enter string to evaluate:");
+        System.out.println("Enter expression to evaluate:");
         exp = input.next();
         System.out.println(Calculator.evaluate(exp));
 
