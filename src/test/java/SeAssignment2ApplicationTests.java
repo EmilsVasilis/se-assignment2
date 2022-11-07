c6712436a947e8853f85974c95a457d2f914b524
EmilsVasilis <emils5264@gmail.com>
2022-11-07 17:34:08 +0000

    Dummy test

--- src/test/java/SeAssignment2ApplicationTests.java
+++ src/test/java/SeAssignment2ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SeAssignment2ApplicationTests {
+class SeAssignment2ApplicationTest {
 
     @Test
     void contextLoads() {
---
+++ src/test/java/calcTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class calcTest {
+
+    @Test
+    public void addTest(){
+        assertEquals(10, calc.add(5,5));
+    }
+}
