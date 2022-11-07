30204409232b1a5cdbc375355ae42fefae0e9685
EmilsVasilis <emils5264@gmail.com>
2022-11-07 17:28:52 +0000

    Fixed file structure

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,6 +1,12 @@
-public class hello {
 
- public static String hello(){
-   return "Hello";
- }
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public  class hello {
+    @GetMapping("/hello")
+    public String index(){
+        return "Greetings";
+    }
 }
